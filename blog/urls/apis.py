1f75f15234b951d1c2f36abf68b1dcb2a9a74202
mahammadkk <kocerlimehemmed2003@gmail.com>
2024-07-11 14:24:14 +0400

    blog details

--- blog/urls/apis.py
+++ blog/urls/apis.py
@@ -4,7 +4,7 @@ from blog.apis import *
 
 
 urlpatterns = [
-    path("crud/", BlogApiView.as_view(), name="blog-get-create-put-delete"),
+    path("blogs/", BlogApiView.as_view(), name="blog-get-create-put-delete"),
     path('blog/<int:pk>/', blog_detail_view, name='blog_detail'),
-    path("crud/<int:pk>/", DeleteBlogApiView.as_view(), name="blog-delete"),
+    path("blog/<int:pk>/", DeleteBlogApiView.as_view(), name="blog-delete"),
 ]
